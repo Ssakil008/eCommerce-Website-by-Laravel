2c1712bb83ed422da6701053ad1a61ef0b6075f2
Monir_Uddin <moniruddinssakil@gmail.com>
2023-11-03 09:54:07 +0600

    login done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            Panel content 
+        </div>
+        <div class="panel-footer">Panel footer</div>
+    </div>
+</body>
+</html>
